8ba7869cc302560e1bd7a404437d861a105da2ff
Raymond Roeming <ray.roeming@gmail.com>
2024-09-03 15:34:46 -0500

    Improves build script name
    Signed-off-by: Raymond Roeming <ray.roeming@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import path from 'path';
 export default defineConfig({
   build: {
     sourcemap: true,
-	outDir: path.resolve(__dirname, "lib"),
+    outDir: path.resolve(__dirname, "lib"),
     lib: {
       entry: path.resolve(__dirname, 'src/TickerGraph.ts'),
       name: 'TickerGraph',
-	  fileName: (type) => type === 'umd' ? "TickerGraph.js" : `TickerGraph.${type}.js`,
+      fileName: (type) => type === 'umd' ? "TickerGraph.js" : `TickerGraph.${type}.js`,
       formats: ['umd', 'es'],
     },
-	minify: false,
+    minify: false,
     rollupOptions: {
       output: {
         name: 'TickerGraph'
